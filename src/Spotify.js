b509218ae55b08148fab357fea06ee46b9521bd7
Erik Cunha <erik.cunha@venturus.org.br>
2019-09-06 16:29:35 -0300

    Add album info

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,7 +1,7 @@
 import SpotifyWrapper from 'spotify-wrapper';
 
 const spotify = new SpotifyWrapper({
-  token: 'YOUR_TOKEN_HERE',
+  token: 'BQBpVU1fhG9d-ImtofmLNpUf-rXc2Bl6T2q6JCHXLNBkl2Yy1LxXzm09D9C2H59o71M2slinQGfrAOCCenaFyP5zXGMDeXUvyYlQG2hYXBohRFZTCkWulUrA5mSWWfGjt_iUQ4A3L0oizMdkNAiDtisqNd-ZbnS_y9aTH7y-P-LA9R4hrf-8PaOG3ry4P34ZzVBwUhRFHgkvExD6gn4zEJ3FLHWawqKcSdxX-96NIb9mrqZSv_Pobg1salh59kDzaAqmCyJaUxGLpw',
 });
 
 export default spotify;
--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-console.log('oi');
+import spotify from './Spotify';
+import renderAlbums from './albumList';
+
+const albums = spotify.search.albums('Incubus');
+
+// eslint-disable-next-line no-undef
+const albumList = document.getElementById('album-list');
+
+albums
+  .then(data => renderAlbums(data.albums.items, albumList));
